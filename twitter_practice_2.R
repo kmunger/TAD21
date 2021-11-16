3e90db9ef07f04eed7b4a05834a0333ca8459b32
Kevin Munger <kevinmunger@gmail.com>
2021-11-16 16:34:32 -0500

    add

---
+++ twitter_practice_2.R
@@ -0,0 +1,27 @@
+##### R Task
+
+
+## Find the 5 Republican and 5 Democrats in Congress with the most Twitter followers
+
+
+## scrape their most recent 200 tweets 
+
+
+### Create a DFM from the text of these tweets
+
+
+### Train a naive bayes classifier to predict the label of the tweets (use all the documents)
+
+
+### Which terms are most predictive?
+
+
+### Now find a political commentator on the left and right (eg Rachel Maddow and Tucker Carlson)
+
+##download their 200 most recent tweets -- use the model to predict whether they are D or R
+
+
+
+## was it correct?  
+
+
--- unsupervised_3.R
+++ unsupervised_3.R
@@ -43,6 +43,15 @@ plot(prep, "rating", model = blog_stm,
      method = "difference", cov.value1 = "Conservative", cov.value2 = "Liberal")
 
 
+# A summary plot of the topics that ranks them by their average proportion in the corpus
+plot(blog_stm, type = "summary")
+
+# A visualization of what words are shared and distinctive to two topics
+plot(blog_stm, type="perspectives", topics = c(1,2))
+
+
+
+
 
 ######modification
 
@@ -58,7 +67,38 @@ plot(prep, "rating", model = blog_stm,
 ##plot using "perspectives" the top two topics and see which words are most distinctive
 
 
+# Load data
+blm_tweets <- read.csv("blm_samp.csv", stringsAsFactors = F)
+
+# Create date vectors
+blm_tweets$datetime <- as.POSIXct(strptime(blm_tweets$created_at, "%a %b %d %T %z %Y",tz = "GMT")) # full date/timestamp
+blm_tweets$date <- mdy(paste(month(blm_tweets$datetime), day(blm_tweets$datetime), year(blm_tweets$datetime), sep = "-")) # date only
+
+# Collapse tweets so we are looking at the total tweets at the day level
+blm_tweets_sum <- blm_tweets %>% group_by(date) %>% summarise(text = paste(text, collapse = " "))
+
+# Remove non ASCII characters
+blm_tweets_sum$text <- stringi::stri_trans_general(blm_tweets_sum$text, "latin-ascii")
+
+# Removes solitary letters
+blm_tweets_sum$text <- gsub(" [A-z] ", " ", blm_tweets_sum$text)
+
+# As always we begin with a DFM.
+# Create DFM
+blm_dfm <-dfm(blm_tweets_sum$text, stem = F, remove_punct = T, tolower = T, remove_twitter = T, remove_numbers = TRUE, remove = c(stopwords("english"),"¦","€","¥", "¯","~", "http","https","rt", "t.co"))
+              
+              
+
+blm_dfm<-dfm_trim(blm_dfm, min_docfreq = 30)
+
+s<-stm(blm_dfm, K = 0) 
 
 
+
+# A summary plot of the topics that ranks them by their average proportion in the corpus
+plot(s, type = "summary")
+
+# A visualization of what words are shared and distinctive to two topics
+plot(s, type="perspectives", topics = c(28,7))
 
 
